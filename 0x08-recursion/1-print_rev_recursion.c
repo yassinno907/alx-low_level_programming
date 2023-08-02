e87307ea3b241c5d9275c6e2c34743ffe1139110
yassinno907 <yassine.nopics95@gmail.com>
2023-08-02 11:54:36 -0700

    a

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print a string.
+ * _print_rev_recursion - print a string.
  * @s: pointer to char
  * Return: No.
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
@@ -14,7 +14,7 @@ void _puts_recursion(char *s)
 	}
 	else
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
